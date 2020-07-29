a88d916f8ecf88844312fe266702b87b1f2da936
joshisiddhant <68951765+joshisiddhant@users.noreply.github.com>
2020-07-29 17:04:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>E Commerce site</title>
+        
+        <link rel="stylesheet" href="style/style.css">
+        <script src="https://kit.fontawesome.com/74f12b28c5.js" crossorigin="anonymous"></script>
+
+    </head>
+
+	<body>
+        <div id="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offer">
+                        <a href="#" class="btn btn-success btn-sm"> Welcome Guest</a> 
+                        <a href="#"> Shopping Cart Total Price: INR 100, Total Items 2</a>
+                    </div>
+                
+                    <div class="col-md-6">
+                        <ul class="menu">
+                            <li>
+                                <a href="customerRegister.php"> Register</a>
+                            </li>
+                            <li>
+                                <a href="checkout.php"> My Account</a>
+                            </li>
+                            <li>
+                                <a href="cart.php"> Cart</a>
+                            </li>
+                            <li>
+                                <a href="login.php"> Login</a>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>    
+        
+        
+        
+		
+            
+	</body>
+</html>
